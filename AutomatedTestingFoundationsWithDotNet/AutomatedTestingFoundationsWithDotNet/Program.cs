67595f52c80aae616ec9b670e70b0c8becf9c571
Ulugbek <ubakhromov22@gmail.com>
2023-07-06 13:04:31 +0500

    Create a program class

---
+++ AutomatedTestingFoundationsWithDotNet/AutomatedTestingFoundationsWithDotNet/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AutomatedTestingFoundationsWithDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarPark carPark = new CarPark();
+            carPark.PrintVehicleInformation();
+        }
+    }
+}
